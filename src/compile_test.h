c82b47d9215290360dab97f1a50a4c07918de161
seemapankhania <seemapankhania@gmail.com>
2018-08-07 14:08:28 +0100

    this is our first commit

--- src/compile_test.h
+++ src/compile_test.h
@@ -19,7 +19,7 @@ namespace bdm {
 
   // Define my custom cell MyCell, which extends Cell by adding extra data members: cell_colour
 BDM_SIM_OBJECT(MyCell, bdm::Cell) { // our object extends the Cell object
-  BDM_SIM_OBJECT_HEADER(MyCellExt, 1, cell_color_); // create the header with our new data member
+  BDM_SIM_OBJECT_HEADER(MyCellExt, 1, cell_color_, prev_movement_); // create the header with our new data member
 
   public:
     MyCellExt() {}
@@ -29,9 +29,13 @@ BDM_SIM_OBJECT(MyCell, bdm::Cell) { // our object extends the Cell object
     int GetCellColor() { return cell_color_[kIdx]; }
     int* GetCellColorPtr() { return cell_color_.data(); }
 
+    // void SavePositionUpdate(std::array<double, 3> prev_movement) {prev_movement_[kIdx] = prev_movement; }
+    // std::array<double,3> GetPositionUpdate() { return prev_movement_[kIdx]; }
+
   private:
   // private data can only be accessed by public function and not directly
     vec<int> cell_color_; // declare our new data member and define its type
+    vec<std::array<double,3>> prev_movement_;
 };
 
 
@@ -48,8 +52,18 @@ struct GrowthModule : public BaseBiologyModule {
     //if (cell->GetDiameter() < 8) {
           //cell->ChangeVolume(400);
 
-          array<double, 3> cell_movements{random->Uniform(-2, 2), random->Uniform(-2, 2), random->Uniform(-2, 2)}; // create an array of 3 random numbers between -2 and 2
+          array<double, 3> cell_movements{random->Uniform(-20, 20), random->Uniform(-20, 20), random->Uniform(-20, 20)}; // create an array of 3 random numbers between -2 and 2
+
+
           cell->UpdatePosition(cell_movements); // update the cell mass location, ie move the cell
+
+          // array<double, 3> last_movement = cell->GetPositionUpdate();
+          // array<double, 3> curr_movement = {0.5*last_movement[0]+0.02*cell_movements[0],0.5*last_movement[1]+0.02*cell_movements[1],0.5*last_movement[2]+0.02*cell_movements[2]};
+          // cell->SavePositionUpdate(curr_movement);
+          // cell->UpdatePosition(curr_movement);
+
+
+          //cell->SavePositionUpdate(cell_movements);
           // cell->SetPosition(cell->GetPosition()); // set the cell position
           //cell->SetCellcolour(cell->GetCellcolour());
 
@@ -98,6 +112,7 @@ inline int Simulate(int argc, const char** argv) {
     cell.SetDiameter(7);
     cell.AddBiologyModule(GrowthModule());
     cell.SetCellColor(4);
+    //cell.SavePositionUpdate({0.0,0.0,0.0});
 
     //cell.SetCellColor(static_cast<int>((y_coord / param->max_bound_ * 6)));
     // will vary from 0 to 5. so 6 different layers depending on y_coord
